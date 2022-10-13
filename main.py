4c15c5ffd7a6ea011ea54aa41e213cbd3965a6bf
amosr2002 <77814810+amosr2002@users.noreply.github.com>
2022-10-13 17:01:23 -0400

    File Includes the finalized model and code

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ from scikeras.wrappers import KerasClassifier
 from sklearn.metrics import confusion_matrix
 from tensorflow.keras import datasets, layers, models
 import seaborn as sns
+import tensorflow as tf
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
 
 
 # PLS DO NOT EXCEED THIS TIME LIMIT
@@ -30,9 +33,9 @@ parser = ArgumentParser()
 # Hint: Advanced optimizer will achieve better performance.
 # Hint: Large Epochs will achieve better performance.
 # Hint: Large Hidden Size will achieve better performance.
-parser.add_argument("--optimizer", default='sgd', type=str)
-parser.add_argument("--epochs", default=75, type=int)
-parser.add_argument("--hidden_size", default=32, type=int)
+parser.add_argument("--optimizer", default='Adam', type=str)
+parser.add_argument("--epochs", default=80, type=int)
+parser.add_argument("--hidden_size", default=64, type=int)
 parser.add_argument("--scale_factor", default=1, type=float)
 ###########################MAGIC ENDS HERE##########################
 
@@ -107,6 +110,8 @@ if args.is_pic_vis:
 # Scale the data attributes 
 # Hint: Scaling the data in the range 0-1 would achieve better results.
 
+
+##MinMax Scaler consistently Achieved us better accuracy than the Default Scaler
 scaler = MinMaxScaler(feature_range=(0,args.scale_factor))
 
 x_train = scaler.fit_transform(x_train.reshape(-1, x_train.shape[-1])).reshape(x_train.shape)
@@ -135,16 +140,73 @@ model = Sequential()
 
 # Build up a neural network to achieve better performance.
 # Hint: Deeper networks (i.e., more hidden layers) and a different activation function may achieve better results.
-model.add(Flatten())
-model.add(Dense(args.hidden_size, activation="relu")) # first layer
-# model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
-# model.add(layers.MaxPooling2D((2, 2)))
-# model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-# model.add(layers.MaxPooling2D((2, 2)))
-# model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+
+
+##Test accuracy of 0.27, only used 15 Epochs Took A Long Time to Train
+# model.add(layers.Conv2D(84, (3, 3)))
+# model.add(Activation('relu'))
+# model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+# model.add(Flatten()) 
+# model.add(Dense(84))
+# model.add(Activation('relu'))
+# model.add(layers.Dropout(0.5))
+
+
+## Test Accuracy 0.41, but took way too long to Train, Used 100 epochs
+# model.add(layers.SeparableConv2D(64, (3, 3), input_shape=(32,32,1)))
+# model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+# model.add(layers.Flatten())
+# model.add(layers.Dense(64, activation='selu'))
+
+## Test Accuracy 0.1977, Used Adadelta Activation Function
+# model.add(layers.Flatten())
+# model.add(layers.Dense(64, activation='selu'))
+# model.add(layers.Dense(64, activation='selu'))
+
+
+##Test Accuracy 0.33, Used SGD Optimizer
+# model.add(layers.Flatten())
+# model.add(layers.Dense(64, activation='relu'))
+# model.add(layers.Dense(64, activation='relu'))
+# model.add(layers.Dense(64, activation='relu'))
+# model.add(layers.Dense(64, activation='relu'))
+
+##Test Accuracy 0.39, Used Adam Optimizer
+# model.add(layers.Flatten())
+# model.add(layers.Dense(63, activation='selu'))
+# model.add(layers.Dense(84, activation='selu'))
+# model.add(layers.Dense(90, activation='tanh'))
+# model.add(layers.Dense(110, activation='relu'))
+
+
+#Test Accuracy 0.38, Used Adam Optimizer
+# model.add(layers.Flatten())
+# model.add(layers.Dense(64, activation='relu'))
+# model.add(layers.Dense(64, activation='relu'))
+# model.add(layers.Dense(64, activation='relu'))
+# model.add(layers.Dense(64, activation='relu'))
+
+
+##Test Accuracy 0.401, Used Adam Optimizer, MinMax Achieved 0.01 better Accuracy than the Default Scaler
+# model.add(layers.Flatten())
+# model.add(layers.Dense(94, activation='selu'))
+# model.add(layers.Dense(94, activation='selu'))
+# model.add(layers.Dense(94, activation='selu'))
+# model.add(layers.Dense(94, activation='selu'))
+
+#Test Accuracy 0.405, Used Adam Optimizer, MinMax Achieved 0.01 better Accuracy than the Default Scaler
+# Weighted Precision Score:  0.4034959726568194
+# WeightedRecall Score:  0.4048
+model.add(layers.Flatten())
+model.add(layers.Dense(args.hidden_size, activation='selu'))
+model.add(layers.Dense(args.hidden_size, activation='selu'))
+model.add(layers.Dense(args.hidden_size, activation='selu'))
+model.add(layers.Dense(args.hidden_size, activation='selu'))
+
 
 ###########################MAGIC ENDS HERE##########################
-model.add(Dense(num_labels)) # last layer
+model.add(layers.Dense(num_labels, activation='softmax')) # last layer
+
 
 # Compile Model
 model.compile(optimizer=args.optimizer,
@@ -158,7 +220,7 @@ history = model.fit(x_train, y_train,
                     epochs=args.epochs,
                     batch_size=512)
 print(history.history)
-
+print(model.summary())
 # Report Results on the test datasets
 test_loss, test_acc = model.evaluate(x_test,  y_test, verbose=2)
 print("\nTest Accuracy: ", test_acc)
@@ -169,7 +231,7 @@ assert end_time - start_time < MAXIMIZED_RUNNINGTIME, "YOU HAVE EXCEED THE TIME 
 # save the model
 model.save(args.model_output_path + "/" + args.model_nick_name)
 
-'''
+''' 
 4. Visualization and Get Confusion Matrix from test dataset 
 '''
 
@@ -179,6 +241,9 @@ from sklearn.metrics import precision_score, recall_score
 import matplotlib.pyplot as plt
 
 ###########################MAGIC HAPPENS HERE##########################
+
+##Attempted Gridsearch, but was too Time Expensive
+
 # optimizer = ['SGD', 'RMSprop', 'Adagrad', 'Adadelta', 'Adam', 'Adamax', 'Nadam']
 # param_grid = dict(optimizer=optimizer)
 # keras_clf = KerasClassifier(model = model, epochs=100, batch_size=10, verbose=0)
@@ -196,6 +261,7 @@ import matplotlib.pyplot as plt
 # Report the precision and recall for 10 different classes
 # Hint: check the precision and recall functions from sklearn package or you can implement these function by yourselves.
 
+#Train/Validation Curve
 plt.plot(history.history['accuracy'])
 plt.plot(history.history['val_accuracy'])
 plt.title('model accuracy')
@@ -204,9 +270,33 @@ plt.xlabel('epoch')
 plt.legend(['train', 'test'], loc='upper left')
 plt.show()
 
+
+#Confusion Matrix
 conf_matrix = confusion_matrix(y_test, y_test_predict)
 sns.heatmap(conf_matrix)
 plt.show()
+
+
+
+#This fetches the indices where the true and predicted images are not equal
+not_equal = np.not_equal(y_test.flatten(), y_test_predict).nonzero()[0]
+not_equal3 = not_equal[:3]
+
+#Shows the first 3 Misclassified Images
+if args.is_pic_vis:
+    plt.figure(figsize=(10,10))
+    for i in not_equal3:
+        plt.subplot()
+        plt.imshow(x_test[i])
+        plt.colorbar()
+        plt.grid(False)
+        plt.xlabel(class_names[y_test[i][0]])
+        plt.show()
+
+
+#Precision and Recall Weighted Scores
+print('Precision Score: ', precision_score(y_test,y_test_predict, average='weighted'))
+print('Recall Score: ', recall_score(y_test,y_test_predict, average='weighted'))
 ###########################MAGIC ENDS HERE##########################
 
 
